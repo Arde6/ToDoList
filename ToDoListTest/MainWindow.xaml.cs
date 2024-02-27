8636ef927c4361198a65c6d6721a76cd650cc84d
Arde6 <artturi.juurenheimo@outlook.com>
2024-02-27 12:28:33 +0200

    Add project files.

---
+++ ToDoListTest/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Collections.Generic;
+using System.Windows;
+
+namespace YourNamespace
+{
+    public partial class MainWindow : Window
+    {
+        private ToDoList toDoList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            toDoList = new ToDoList();
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            string task = taskTextBox.Text;
+            toDoList.AddTask(task);
+            taskListBox.ItemsSource = toDoList.GetTasks();
+            taskTextBox.Clear();
+        }
+    }
+}
+
---
+++ ToDoListTest/ToDoListObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ToDoList
+{
+    private List<string> tasks;
+
+    public ToDoList()
+    {
+        tasks = new List<string>();
+    }
+
+    public void AddTask(string task)
+    {
+        tasks.Add(task);
+    }
+
+    public List<string> GetTasks()
+    {
+        return tasks;
+    }
+}
